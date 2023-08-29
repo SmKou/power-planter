cd14a0be11c69359908992e15d5a20f27a3f8cee
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-28 23:45:04 -0700

    WIP Add resizing garden

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import './css/styles.css';
 
 // Refer variables from .env => ${process.env.varName}
 
+const app = () => ({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    qty: function () { 
+        return Math.floor((this.width / 100) * (this.height / 100))
+    }
+})
+let app_state = app()
+
 const garden = document.querySelector('.garden');
-const plots = Array(25).fill(0);
+const plots = Array(app_state.qty).fill(0);
 
 for (let i = 0; i < plots.length; i++) {
     const plot_div = document.createElement('div');
@@ -12,10 +21,14 @@ for (let i = 0; i < plots.length; i++) {
     garden.append(plot_div);
 }
 
-const plants = [bud, seed, bloom, flower]
+const plants = ["bud", "seed", "bloom", "flower"]
 for (let i = 0; i < plants.length; i++) {
     const plant = document.createElement('div');
     plant.classList.add('plant');
     plant.classList.add(plants[i]);
     document.getElementById(`plot-${i}`).append(plant);
 }
+
+
+window.onload = () => app_state = app()
+window.onresize = () => app_state = app()
