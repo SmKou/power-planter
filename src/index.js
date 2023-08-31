ba43871131afdb704a4854abedf5a6dfd7b1498c
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-30 23:21:47 -0700

    Add node setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import './css/fonts.css';
 // import './css/styles.css';
-const { plants } = require('./plant');
+const { plants } = require('./js/plant');
 
 // Refer variables from .env => ${process.env.varName}
